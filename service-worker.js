b1a15f9976ead35a727bacff137114e9cd7ea4f1
Lucas <lucastuleski@outlook.com>
2026-09-25 16:58:00 -0300

    25-09-2026 16-58-00

--- service-worker.js
+++ service-worker.js
@@ -31,11 +31,19 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      });
+return cached || fetch(event.request).then(response => {
+
+  if (response.status === 200) {
+    const copy = response.clone();
+
+    caches.open(CACHE_NAME).then(cache => {
+      cache.put(event.request, copy);
+    });
+  }
+
+  return response;
+
+});
     }).catch(() => caches.match("./index.html"))
   );
 });
